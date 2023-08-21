b770d90eea8f3c86b8200fb1796383b859f5215e
Łukasz Łopata <lukasz.lopata.developer@gmail.com>
2023-08-21 22:06:40 +0200

    Unit Test project prepared

---
+++ Katharsis.Http.UnitTest/KatharsisClientTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katharsis.Http.UnitTests
+{
+    [TestFixture]
+    public class KatharsisClientTests
+    {
+        private KatharsisClient _client;
+
+        [SetUp]
+        public void Setup()
+        {
+            _client = new KatharsisClient();
+        }
+
+    }
+}
